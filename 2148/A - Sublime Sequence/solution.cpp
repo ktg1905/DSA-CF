c47547b4f66bb89595b6c1c2bc1f0f3524fb00c2
Kratagya Jain <kratagyajain19@gmail.com>
2026-09-04 23:26:51 +0530

    Add Sublime Sequence [A] from Codeforces

---
+++ 2148/A - Sublime Sequence/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        int n;
+        cin>>x>>n;
+ 
+        if(n%2==0){
+            cout<<"0"<<'
+';
+        }
+        else{
+            cout<<x<<'
+';
+        }
+    }
+ 
+    
+ 
+    return 0;
+}
