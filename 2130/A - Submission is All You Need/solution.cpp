87b858cbf42e32e685b1a97792dcc4b2a716484e
Kratagya Jain <kratagyajain19@gmail.com>
2026-09-04 23:30:24 +0530

    Add Submission is All You Need [A] from Codeforces

---
+++ 2130/A - Submission is All You Need/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+ 
+        int num=0;
+        int sum=0;
+        for(int i=0; i<n; i++){
+            int x;
+            cin>>x;
+            if(x==0){
+                num++;
+            }
+            sum+=x;
+        }
+        
+            cout<<sum+num<<'
+';
+        
+        
+    }
+ 
+    
+ 
+    return 0;
+}
