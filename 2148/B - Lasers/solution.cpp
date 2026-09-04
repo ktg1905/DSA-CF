e8c9404d86ef6215cbb3e601fda19f11c3806ea4
Kratagya Jain <kratagyajain19@gmail.com>
2026-09-04 23:26:55 +0530

    Add Lasers [B] from Codeforces

---
+++ 2148/B - Lasers/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        int m;
+        int x;
+        int y;
+ 
+        cin>>n>>m>>x>>y;
+        for(int i=0; i<m+n; i++){
+            int x;
+            cin>>x;
+        }
+        cout<<m+n<<'
+';
+    }
+ 
+    
+ 
+    return 0;
+}
