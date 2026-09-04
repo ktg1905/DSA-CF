4a0854a8664a29324fc5287ca309a389addee472
Kratagya Jain <kratagyajain19@gmail.com>
2026-09-04 23:26:21 +0530

    Add Koshary [A] from Codeforces

---
+++ 2227/A - Koshary/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        int y;
+        cin>>x>>y;
+ 
+        if((x%2!=0)&&(y%2!=0)){
+            cout<<"no"<<'
+';
+        }
+        else{
+            cout<<"yes"<<'
+';
+        }
+    }
+ 
+    
+ 
+    return 0;
+}
