df990e0c2491aad2a5c9dd951790638c46a678e1
Kratagya Jain <kratagyajain19@gmail.com>
2026-09-04 23:30:59 +0530

    Add Blackboard Game [A] from Codeforces

---
+++ 2123/A - Blackboard Game/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+ 
+        if(n%4==0){
+            cout<<"Bob"<<'
+';
+        }
+        else{
+            cout<<"Alice"<<'
+';
+        }
+    }
+ 
+ 
+    
+ 
+    return 0;
+}
