6912203e560f2cf73bb383dbe7fbd425b64f30d2
Kratagya Jain <kratagyajain19@gmail.com>
2026-09-04 23:31:25 +0530

    Add farmpiggie and Subset Sum [A] from Codeforces

---
+++ 2246/A - farmpiggie and Subset Sum/solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+ 
+        vector<int> a(n,0);
+ 
+        for(int i=0; i<n; i=i+2){
+            a[i]=i+2;
+        }
+        for(int i=1; i<n; i=i+2){
+            a[i]=i;
+        }
+        for(int i=0; i<n; i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<'
+';
+    }
+ 
+    
+ 
+    return 0;
+}
