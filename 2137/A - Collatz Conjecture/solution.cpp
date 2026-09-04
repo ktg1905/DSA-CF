1a31b25d2e9dd3be4d614882db46b29d4ee1f2c6
Kratagya Jain <kratagyajain19@gmail.com>
2026-09-04 23:27:58 +0530

    Add Collatz Conjecture [A] from Codeforces

---
+++ 2137/A - Collatz Conjecture/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int k;
+        int x;
+        cin>>k>>x;
+ 
+        cout<<(int)(x*pow(2,k))<<'
+';
+    }
+ 
+    
+ 
+    return 0;
+}
