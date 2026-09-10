a5c6aa983e8cfcd84422109900dc9ec43a5cd633
Kratagya Jain <kratagyajain19@gmail.com>
2026-09-11 01:08:52 +0530

    Add Monocarp's Contest [A] from Codeforces

--- 2260/A - Monocarp's Contest/solution.cpp
+++ 2260/A - Monocarp's Contest/solution.cpp
@@ -1,28 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+ 
     int t;
     cin>>t;
     while(t--){
         int n;
         cin>>n;
- 
+ 
         vector<int> a;
         int num_0=0;
         for(int i=0; i<n; i++){
             int x;
             cin>>x;
- 
+ 
             a.push_back(x);
             if(x==0){
                 num_0++;
             }
         }
- 
+ 
         if(num_0<2){
             cout<<-1<<'
 ';
@@ -46,8 +46,8 @@ int main() {
             }
         }
     }
- 
+ 
     
- 
+ 
     return 0;
 }
