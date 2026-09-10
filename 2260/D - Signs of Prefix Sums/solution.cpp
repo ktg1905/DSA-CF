fed94af00f2b02a1e2c85a554a0fc53da7b0fcbf
Kratagya Jain <kratagyajain19@gmail.com>
2026-09-11 01:11:37 +0530

    Add Signs of Prefix Sums [D] from Codeforces

--- 2260/D - Signs of Prefix Sums/solution.cpp
+++ 2260/D - Signs of Prefix Sums/solution.cpp
@@ -1,63 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+ 
 bool possible(const string &s, int C) {
     bool dp[7] = {};
- 
+ 
     dp[3] = true;
- 
+ 
     for (char c : s) {
         bool ndp[7] = {};
- 
+ 
         for (int prev = -3; prev <= 3; prev++) {
             if (!dp[prev + 3])
                 continue;
- 
+ 
             for (int cur = -3; cur <= 3; cur++) {
             
                 if (cur == prev)
                     continue;
- 
+ 
                 if (abs(cur - prev) > C)
                     continue;
- 
+ 
                 if (c == '+' && cur <= 0)
                     continue;
- 
+ 
                 if (c == '-' && cur >= 0)
                     continue;
- 
+ 
                 if (c == '0' && cur != 0)
                     continue;
- 
+ 
                 ndp[cur + 3] = true;
             }
         }
- 
+ 
         memcpy(dp, ndp, sizeof(dp));
     }
- 
+ 
     for (int v = -3; v <= 3; v++) {
         if (dp[v + 3])
             return true;
     }
- 
+ 
     return false;
 }
- 
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+ 
     int t;
     cin >> t;
- 
+ 
     while (t--) {
         int n;
         string s;
- 
+ 
         cin >> n >> s;
- 
+ 
         if (possible(s, 1))
             cout << 1 << '
 ';
@@ -71,6 +71,6 @@ int main() {
             cout << -1 << '
 ';
     }
- 
+ 
     return 0;
 }
