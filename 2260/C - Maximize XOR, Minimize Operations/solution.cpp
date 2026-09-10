d7dbb0c15c56a0be737028cb12343269188d5fc6
Kratagya Jain <kratagyajain19@gmail.com>
2026-09-11 01:10:28 +0530

    Add Maximize XOR, Minimize Operations [C] from Codeforces

--- 2260/C - Maximize XOR, Minimize Operations/solution.cpp
+++ 2260/C - Maximize XOR, Minimize Operations/solution.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
- 
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+ 
     int t;
     cin>>t;
     while(t--){
@@ -13,7 +13,7 @@ int main() {
         cin>>x>>y;
         ll sum = x+y;
         ll a = 0;
- 
+ 
         for(int bit=29; bit>=0; bit--){
             if(sum & (1LL << bit)){
                 ll prob = a | (1LL << bit);
@@ -22,12 +22,12 @@ int main() {
                 }
             }
         }
- 
+ 
         cout<<sum<<" "<<x-a<<'
 ';
     }
- 
+ 
     
- 
+ 
     return 0;
 }
